946136d6d8191ec5b194c81bfe0df4c46975fb20
2scent <ad7549@gmail.com>
2024-08-01 18:32:43 +0900

    feat: sangtae의 set 함수 구현

--- src/core/sangtae.ts
+++ src/core/sangtae.ts
@@ -1,13 +1,23 @@
 interface Sangtae<T> {
   get: () => T;
+  set: (next: T | ((prev: T) => T)) => void;
 }
 
 export function sangtae<T>(initialState: T): Sangtae<T> {
-  const state = initialState;
+  let state = initialState;
 
   const get = () => state;
 
+  const set = (next: T | ((prev: T) => T)) => {
+    if (typeof next === 'function') {
+      state = (next as (prev: T) => T)(state);
+    } else {
+      state = next;
+    }
+  };
+
   return {
     get,
+    set,
   };
 }
