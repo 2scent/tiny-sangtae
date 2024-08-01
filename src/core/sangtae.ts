0042fd864d3519edfb46aed987d9dd9127de69e5
2scent <ad7549@gmail.com>
2024-08-01 19:27:30 +0900

    feat: sangtae의 subscribe 함수 구현

--- src/core/sangtae.ts
+++ src/core/sangtae.ts
@@ -1,23 +1,42 @@
+type Callback = () => void;
+
 interface Sangtae<T> {
   get: () => T;
   set: (next: T | ((prev: T) => T)) => void;
+  subscribe: (callback: Callback) => () => void;
 }
 
 export function sangtae<T>(initialState: T): Sangtae<T> {
   let state = initialState;
 
+  const callbacks = new Set<Callback>();
+
   const get = () => state;
 
   const set = (next: T | ((prev: T) => T)) => {
+    const prev = state;
+
     if (typeof next === 'function') {
       state = (next as (prev: T) => T)(state);
     } else {
       state = next;
     }
+
+    if (prev !== state) {
+      callbacks.forEach((callback) => callback());
+    }
+  };
+
+  const subscribe = (callback: Callback) => {
+    callbacks.add(callback);
+    return () => {
+      callbacks.delete(callback);
+    };
   };
 
   return {
     get,
     set,
+    subscribe,
   };
 }
