cd59419d50c49ae94cdbf546bd863a372042a14a
2scent <ad7549@gmail.com>
2024-08-03 13:19:47 +0900

    refactor: sangtae 리팩토링
    - 타입 분리
    - forEach 대신 for ... of 사용

--- src/core/sangtae.ts
+++ src/core/sangtae.ts
@@ -1,29 +1,35 @@
+type NextFunction<T> = (prev: T) => T;
+
+type Next<T> = T | NextFunction<T>;
+
 type Callback = () => void;
 
-interface Sangtae<T> {
-  get: () => T;
-  set: (next: T | ((prev: T) => T)) => void;
+interface Sangtae<State> {
+  get: () => State;
+  set: (next: Next<State>) => void;
   subscribe: (callback: Callback) => () => void;
 }
 
-export function sangtae<T>(initialState: T): Sangtae<T> {
+export function sangtae<State>(initialState: State): Sangtae<State> {
   let state = initialState;
 
   const callbacks = new Set<Callback>();
 
   const get = () => state;
 
-  const set = (next: T | ((prev: T) => T)) => {
+  const set = (next: Next<State>) => {
     const prev = state;
 
     if (typeof next === 'function') {
-      state = (next as (prev: T) => T)(state);
+      state = (next as NextFunction<State>)(state);
     } else {
       state = next;
     }
 
     if (prev !== state) {
-      callbacks.forEach((callback) => callback());
+      for (const callback of callbacks) {
+        callback();
+      }
     }
   };
 
